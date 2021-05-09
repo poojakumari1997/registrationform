9b0b2b534935d37b1bb22ba04520871b09ea23d6
Pooja Kumari <pooja.kumari@moolya.com>
2021-05-09 20:15:52 +0530

    db

--- index.php
+++ index.php
@@ -52,8 +52,8 @@ $db = $row['dob'];
 echo " Name: {$na} <br> Contact Number: {$ph} <br> Email ID: {$em} <br> Gender: {$gen} <br> Date Of Birth: {$db}";
 
 }
-$que = "SELECT * FROM abcd;
-$result = mysqli_query($con,$que) or die(mysqli_error($con));
+$qu = "SELECT * FROM abcd;
+$result = mysqli_query($con,$qu) or die(mysqli_error($con));
 while( $row = mysqli_fetch_array($result)){
   echo "<h3>Your Records are:</h3>";
  $na = $row['name'];
