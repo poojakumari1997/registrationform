830cd9ac80d87c3e21c7e796606d749a7c787df9
Pooja Kumari <pooja.kumari@moolya.com>
2021-05-09 20:05:38 +0530

    commit

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php 
 
 
-// require_once('./vendor/autoload.php');
-$con = mysqli_connect("localhost","root","Pooja@123","pooja","3307");
+//  require_once('./vendor/autoload.php');
+$con = mysqli_connect("remotemysql.com","hkc9luRsEi","eLn8JCHMpr","hkc9luRsEi","3306");
 
 if(!$con){
   echo mysqli_connect_error();
   die();
 }
-// echo "Database connected";
-// $sql = "CREATE TABLE forms(
-// name VARCHAR(30) NOT NULL PRIMARY KEY,
-// phone VARCHAR(30) NOT NULL,
-// email VARCHAR(40) NOT NULL,
-// gender VARCHAR(10) NOT NULL,
-// dob VARCHAR(10) NOT NULL
-// )";
+echo "Database connected";
+$sql = "CREATE TABLE abcd(
+name VARCHAR(30) NOT NULL PRIMARY KEY,
+phone VARCHAR(30) NOT NULL,
+email VARCHAR(40) NOT NULL,
+gender VARCHAR(10) NOT NULL,
+dob VARCHAR(10) NOT NULL
+)";
 
-// if(mysqli_query($con, $sql)){
-//   echo "Table Created Successfully";
-// }else{
-//   echo "Error creating table" .mysqli_error($con);
-// }
+if(mysqli_query($con, $sql)){
+  echo "Table Created Successfully";
+}else{
+  echo "Error creating table" .mysqli_error($con);
+}
 
 $Name = $_POST['fname'];
 $Phone = $_POST['phn'];
@@ -29,7 +29,7 @@ $Email = $_POST['mail'];
 $Gender = $_POST['gender'];
 $DOB = $_POST['dateb'];
 
-$sql = "INSERT INTO forms VALUES ('$Name','$Phone','$Email','$Gender','$DOB')";
+$sql = "INSERT INTO abcd VALUES ('$Name','$Phone','$Email','$Gender','$DOB')";
 $rs = mysqli_query($con, $sql);
 if($rs){
   echo "Record Inserted Successfully!";
@@ -39,7 +39,8 @@ if($rs){
 echo "not inserted";
 // mysqli_select_db("pooja");
 
-$que = "SELECT * FROM forms WHERE name= '$Name'";
+
+$que = "SELECT * FROM abcd WHERE name= '$Name'";
 $result = mysqli_query($con,$que) or die(mysqli_error($con));
 while( $row = mysqli_fetch_array($result)){
   echo "<h3>Your Records are:</h3>";
